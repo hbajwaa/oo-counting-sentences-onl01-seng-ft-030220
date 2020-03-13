297003a20e3dc00da54004f90d55bdac144ebcb7
Hassan Bajwa <bajwahassan3@outlook.com>
2020-03-13 05:56:35 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,15 +16,6 @@ class String
 
   def count_sentences
     a = self.split(".").length
-    counter = 1
-    a.each do |sentence|
-      if sentence.include("?") 
-        counter += 1
-      elsif sentence.include("!") 
-        counter += 1  
-      elsif sentence.include(".")
-        counter += 1
-    end
-    counter
+    
   end
 end
